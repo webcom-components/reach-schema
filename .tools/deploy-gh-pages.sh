3c47dac427e65f6d30b001862220be4b823ba3a4
franck <franck.lebris@orange.com>
2018-02-08 11:48:54 +0100

    Try to have git log

--- .tools/deploy-gh-pages.sh
+++ .tools/deploy-gh-pages.sh
@@ -32,4 +32,4 @@ git config user.email "franck.lebris@orange.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 #git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages /dev/null 
